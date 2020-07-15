34bb5f2d5a4aba7112160605abbbf4ee7130f194
Tapio Teras <tapio.teras@reaktor.com>
2020-07-15 15:05:14 +0300

    Implement recipe screen contents

--- src/recipe-screen.tsx
+++ src/recipe-screen.tsx
@@ -1,17 +1,63 @@
 import React from "react";
-import { Text } from "@chakra-ui/core";
+import { List, ListItem, Text, Divider } from "@chakra-ui/core";
 import { RecipeProps } from "./recipes-screen";
 import { useParams, useLocation } from "react-router-dom";
 
-interface RecipeScreenProps extends RecipeProps {}
+export interface RecipeScreenProps extends RecipeProps {
+  name: string;
+  description?: string;
+  portions?: string;
+  ingredients: IngredientRowProps[];
+  steps?: string[];
+}
+
+export interface IngredientRowProps {
+  name: string;
+  unit?: string;
+  amount?: number;
+}
+
+const IngredientRow: React.FC<IngredientRowProps> = ({
+  name,
+  unit,
+  amount,
+}) => (
+  <>
+    {amount && amount}
+    {unit && ` ${unit}`} {name.toLowerCase()}
+  </>
+);
 
 const RecipeScreen: React.FC<RecipeScreenProps> = () => {
   const { id } = useParams();
-  const { state } = useLocation();
+  const {
+    state: {
+      recipe: { name, description, portions, ingredients, steps },
+    },
+  } = useLocation();
   return (
     <>
-      <Text fontSize="6xl">{`Selected recipe ${id}`}</Text>
-      <Text fontSize="4xl">{JSON.stringify(state)}</Text>
+      <Text fontSize="6xl">{name}</Text>
+      {description && <Text fontSize="2xl">{description}</Text>}
+      {portions && (
+        <Text fontSize="2xl">{`${portions} ${
+          portions == 1 ? "annos" : "annosta"
+        }.`}</Text>
+      )}
+      <Divider />
+      <List styleType="disc">
+        {[...ingredients].map((ingredient) => (
+          <ListItem fontSize="xl">
+            <IngredientRow {...ingredient} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List as="ol" styleType="decimal">
+        {[...steps].map((step) => (
+          <ListItem>{step}</ListItem>
+        ))}
+      </List>
     </>
   );
 };
