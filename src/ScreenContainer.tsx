4b87f88f0642d2e644b2a9ba0602bb2e60245ab1
Tapio Teras <tapio.teras@reaktor.com>
2020-07-17 10:53:35 +0300

    Change base color to also for body bg

--- src/ScreenContainer.tsx
+++ src/ScreenContainer.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Box, Stack } from "@chakra-ui/core";
+import { Box, Flex, Stack } from "@chakra-ui/core";
 
 export interface ScreenContainerProps {
   children: React.ReactNode;
 }
 
 const ScreenContainer: React.FC<ScreenContainerProps> = ({ children }) => (
-  <Box bg="tomato" w="100%" p={4} color="white">
-    <Stack paddingTop={5} spacing={5}>
-      {children}
-    </Stack>
-  </Box>
+
+    <Box bg="red[400]" w="100%" p={4} color="white">
+      <Stack paddingTop={5} spacing={5}>
+        {children}
+      </Stack>
+    </Box>
 );
 
 export default ScreenContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import recipes from "../mock/test-data";
 import categories from "../mock/categories";
-import { CSSReset, ThemeProvider, theme } from "@chakra-ui/core";
+import { CSSReset, ThemeProvider, theme, ITheme } from "@chakra-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import RecipeScreen from "./recipe-screen";
 import RecipesScreen from "./recipes-screen";
 
 const themeOverrides = {
   ...theme,
-  breakpoints: ["30em", "48em", "62em", "80em"],
   fonts: {
     heading: '"Avenir Next", sans-serif',
     body: "system-ui, sans-serif",
@@ -17,17 +16,18 @@ const themeOverrides = {
   },
   colors: {
     ...theme.colors,
-    brand: {
-      900: "#1a365d",
-      800: "#153e75",
-      700: "#2a69ac",
-    },
   },
 };
 
 ReactDOM.render(
   <ThemeProvider theme={themeOverrides}>
-    <CSSReset />
+    <CSSReset
+      config={(theme: ITheme) => ({
+        light: {
+          bg: theme.colors.red[400],
+        },
+      })}
+    />
     <Router>
       <Switch>
         <Route path="/recipe/:id">
