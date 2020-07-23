6fb91066d661fd9717a82f973bd456a4ea6572bf
Tapio Teras <tapio.teras@reaktor.com>
2020-07-23 20:05:14 +0300

    Try cors proxy to avoid server side setup

--- src/api/KRuokaApi.ts
+++ src/api/KRuokaApi.ts
@@ -1,6 +1,6 @@
 class KRuokaApi {
   public async searchRecipes(search: string) {
-    await fetch(`https://www.k-ruoka.fi/kr-api/search?offset=0&q=${search}`, {
+    await fetch(`https://cors-anywhere.herokuapp.com/https://www.k-ruoka.fi/kr-api/search?offset=0&q=${search}`, {
       mode: "cors",
     });
   }
