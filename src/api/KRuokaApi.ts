11791c443e48f626600e89f557213ad6055d909a
Tapio Teras <tapio.teras@reaktor.com>
2020-07-24 10:07:02 +0300

    Add note about cors stuff

--- src/api/KRuokaApi.ts
+++ src/api/KRuokaApi.ts
@@ -5,6 +5,7 @@ class KRuokaApi {
     onFailure: (error) => void
   ) {
     fetch(
+      // TODO cors-anywhere is meant to be used only for DEBUGGING purposes! plz remove cors at some point
       `https://cors-anywhere.herokuapp.com/https://www.k-ruoka.fi/kr-api/search?offset=0&q=${search}`,
       {
         mode: "cors",
