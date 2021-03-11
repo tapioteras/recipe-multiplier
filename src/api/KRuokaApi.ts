fbfc003efe2a84d2bf64c0982d557217d0c5ece7
Tapio Teras <tapio.teras@reaktor.com>
2021-03-11 09:01:49 +0200

    Move cors provider root path and fetch options

--- src/api/KRuokaApi.ts
+++ src/api/KRuokaApi.ts
@@ -1,3 +1,9 @@
+// TODO cors is meant to be used only for DEBUGGING purposes! plz remove cors at some point
+const corsProviderForDebuggingUseOnly = "https://thingproxy.freeboard.io/fetch/"
+
+const options: RequestInit = {
+  mode: "cors",
+}
 class KRuokaApi {
   public searchRecipes(
     search: string,
@@ -5,19 +11,15 @@ class KRuokaApi {
     onFailure: (error) => void
   ) {
     fetch(
-      // TODO cors is meant to be used only for DEBUGGING purposes! plz remove cors at some point
-      `https://thingproxy.freeboard.io/fetch/https://www.k-ruoka.fi/kr-api/search?offset=0&q=${search}`,
-      {
-        mode: "cors",
-      }
+      `${corsProviderForDebuggingUseOnly}https://www.k-ruoka.fi/kr-api/search?offset=0&q=${search}`,
+      options
     )
       .then((response) => response.json())
       .then(onSuccess)
       .catch(onFailure);
   }
   fetchRecipe(url, onSuccess: (data) => void, onFailure: (error) => void) {
-    // TODO cors is meant to be used only for DEBUGGING purposes! plz remove cors at some point
-    fetch(`https://thingproxy.freeboard.io/fetch/${url}`)
+    fetch(`${corsProviderForDebuggingUseOnly}${url}`, options)
       .then((response) => response.text())
       .then(onSuccess)
       .catch(onFailure);
